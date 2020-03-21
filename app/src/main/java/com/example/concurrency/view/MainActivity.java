82536d2eab52d6a484d239a342d26c2167639298
dgdanski <>
2020-03-21 17:27:38 +0100

    New directory

--- app/src/main/java/com/example/concurrency/view/MainActivity.java
+++ app/src/main/java/com/example/concurrency/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.concurrency;
+package com.example.concurrency.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.concurrency.R;
 
 public class MainActivity extends AppCompatActivity {
 
