47ce57fcad6f619c9c5cc2d2de46eadad2023916
dgdanski <>
2020-03-21 17:50:47 +0100

    Get request for currencies rate

--- app/src/main/java/com/example/concurrency/view/MainActivity.java
+++ app/src/main/java/com/example/concurrency/view/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.concurrency.view;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.concurrency.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +21,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private static class MarketDataAsyncTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String response = null;
+            try {
+                String baseCurrency = params[0];
+                URL url = new URL("https://hiring.revolut.codes/api/android/latest?base=" + baseCurrency);
+                HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+                httpConn.setRequestMethod("GET");
+                InputStream inputStream = httpConn.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                response = bufferedReader.readLine();
+                httpConn.disconnect();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            Log.d("RESPONSE", response);
+        }
     }
 }
