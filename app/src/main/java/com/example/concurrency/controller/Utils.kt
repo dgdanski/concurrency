4c63561c16d4cec8618ae2e639465d9677e5161a
dgdanski <>
2020-03-22 18:06:08 +0100

    Utils method to hide keyboard

--- app/src/main/java/com/example/concurrency/controller/Utils.kt
+++ app/src/main/java/com/example/concurrency/controller/Utils.kt
@@ -1,11 +1,15 @@
 package com.example.concurrency.controller
 
+import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.google.gson.Gson
 import java.math.BigDecimal
 import java.math.RoundingMode
+
 
 class Utils {
     companion object {
@@ -37,6 +41,18 @@ class Utils {
             var bigDecimal = BigDecimal.valueOf(value)
             bigDecimal = bigDecimal.setScale(decimalPoint, RoundingMode.HALF_UP)
             return bigDecimal.toDouble()
+        }
+
+        @JvmStatic
+        fun hideKeyboard(activity: Activity) {
+            val inputMethodManager: InputMethodManager = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            //Find the currently focused view, so we can grab the correct window token from it.
+            var view: View? = activity.currentFocus
+            //If no view currently has focus, create a new one, just so we can grab a window token from it
+            if (view == null) {
+                view = View(activity)
+            }
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
 }
