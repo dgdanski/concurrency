df2094e80f08168cac0cf80c8f8011a7497c483f
dgdanski <>
2020-04-08 01:11:21 +0200

    Add observer to every recycler item

--- app/src/main/java/com/example/concurrency/view/recyclerView/currency/CurrencyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/concurrency/view/recyclerView/currency/CurrencyRecyclerViewAdapter.java
@@ -2,16 +2,23 @@ package com.example.concurrency.view.recyclerView.currency;
 
 import android.app.Activity;
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.concurrency.R;
 import com.example.concurrency.controller.Utils;
+import com.example.concurrency.model.CurrencyItemViewModel;
 import com.example.concurrency.model.CurrencyMarketDataWrapper;
 
 import org.jetbrains.annotations.NotNull;
@@ -25,10 +32,10 @@ import static com.example.concurrency.controller.Utils.round;
 public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyRecyclerViewAdapter.ViewHolder> {
 
     private LayoutInflater mInflater;
-    private ItemClickListener mClickListener;
     private LinkedList<Map.Entry<String, Double>> ratesList = new LinkedList<>();
     private RecyclerView recyclerView;
     private Context context;
+    private CurrencyItemViewModel currencyItemViewModel;
 
     // data is passed into the constructor
     public CurrencyRecyclerViewAdapter(Context context, CurrencyMarketDataWrapper currencyMarketDataWrapper, RecyclerView recyclerView) {
@@ -40,6 +47,7 @@ public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyRe
     }
 
     private EditText editTextFromFirstItem;
+    private Double firstItemRate;
 
     public void updateRates(CurrencyMarketDataWrapper newMarketData) {
         for (Map.Entry<String, Double> entry : ratesList) {
@@ -50,11 +58,13 @@ public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyRe
         editTextFromFirstItem.setText(String.valueOf(round(firstItemRate * 1, 2)));
         editTextFromFirstItem.setSelection(cursorPosition);
     }
+
     // inflates the row layout from xml when needed
     @NotNull
     @Override
     public ViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int viewType) {
         View view = mInflater.inflate(R.layout.currency_recycler_view_single_item, parent, false);
+        currencyItemViewModel = new ViewModelProvider((AppCompatActivity) context).get(CurrencyItemViewModel.class);
         return new ViewHolder(view, recyclerView);
     }
 
@@ -65,7 +75,51 @@ public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyRe
         holder.currencyTickerTextView.setText(currencyFullName);
         holder.currencyFullNameTextView.setText(currencyFullName);
         Double rate = ratesList.get(position).getValue();
-        holder.currencyValueEditText.setText(String.valueOf(round(rate * 1, 2)));
+        if (position == 0) {
+            editTextFromFirstItem = holder.currencyValueEditText;
+            firstItemRate = rate;
+            holder.currencyValueEditText.setText(String.valueOf(round(rate * 1, 2)));
+        } else if (position == 1) {
+            holder.currencyValueEditText.setText(String.valueOf(round(rate * firstItemRate, 2)));
+        } else {
+            holder.currencyValueEditText.setText(String.valueOf(round(rate * firstItemRate, 2)));
+        }
+
+        subscribe(holder.currencyValueEditText, position);
+    }
+    String previousValue;
+    private void subscribe(EditText editText, int position) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (position == 0 && !s.toString().equals(previousValue)) {
+                    currencyItemViewModel.firstItemValue.setValue(Double.valueOf(s.toString()));
+                    firstItemRate = Double.valueOf(s.toString());
+                    previousValue = s.toString();
+                }
+            }
+        });
+
+        currencyItemViewModel.firstItemValue.observe((AppCompatActivity) context, new Observer<Double>() {
+            @Override
+            public void onChanged(@Nullable Double value) {
+                Double rateFromFirstItem = Double.valueOf(editTextFromFirstItem.getText().toString());
+                Double ownRate = ratesList.get(position).getValue();
+                if (position != 0) {
+                    editText.setText(String.valueOf(round(ownRate * rateFromFirstItem, 2)));
+                }
+            }
+        });
     }
 
     // total number of rows
@@ -74,6 +128,20 @@ public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyRe
         return ratesList.size();
     }
 
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+//    @Override
+//    public int getItemViewType(int position) {
+//        return position;
+//    }
 
     // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -100,7 +168,6 @@ public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyRe
 
         @Override
         public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
             Utils.hideKeyboard((Activity) context);
             clearFocusOfTheFirstItem();
             moveItemToTop(getAdapterPosition());
@@ -123,20 +190,5 @@ public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyRe
         ratesList.add(0, removedItem);
         notifyItemMoved(itemCurrentPosition, 0);
         recyclerView.scrollToPosition(0);
-    }
-
-    // convenience method for getting data at click position
-    public String getItem(int id) {
-        return String.valueOf(id);
-    }
-
-    // allows clicks events to be caught
-    public void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
-
-    // parent activity will implement this method to respond to click events
-    public interface ItemClickListener {
-        void onItemClick(View view, int position);
     }
 }
